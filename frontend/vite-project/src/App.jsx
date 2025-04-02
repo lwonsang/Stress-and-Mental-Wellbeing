c0434e3efe1e1d52fe0574daa3c2750f10efda95
J1ngy1 <jingyi.xu@wustl.edu>
2025-04-02 12:03:30 -0500

    Init

---
+++ frontend/vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h1>Stress and Mental Wellbeing</h1>
+    </div>
+  );
+}
+
+export default App

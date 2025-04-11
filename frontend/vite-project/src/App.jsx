8d477d040bca19fd342a9f247c1b9eec663e5cbf
J1ngy1 <jingyi.xu@wustl.edu>
2025-04-10 22:21:16 -0500

    fix: github page routes

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -10,14 +10,18 @@ import Calendar from "./components/MonthlyView";
 import { useEffect } from "react";
 
 function App() {
+  const isProd = import.meta.env.MODE === "production";
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/weekly" element={<WeeklyPage />} />
-        <Route path="/monthly" element={<Calendar />} />
-      </Routes>
-    </Router>
+
+<Router basename={isProd ? "/Stress-and-Mental-Wellbeing" : "/"}>
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/weekly" element={<WeeklyPage />} />
+    <Route path="/monthly" element={<Calendar />} />
+  </Routes>
+</Router>
+
   );
 }
 
